11d5a0304b4a247fc2524140b5e91f33dfc0447d
duonglmph41308 <duongluong2k4@gmail.com>
2024-01-27 21:32:46 +0700

    duong up sanpham Admin

---
+++ admin/sanpham/update.php
@@ -0,0 +1,69 @@
+
+<?php
+if(is_array($sanpham)){
+    extract($sanpham);
+
+}
+$hinhpath = "../upload/".$img;
+       $hinh = "";
+       if(is_file($img)){
+           $hinh = "<img src='".$hinhpath."' height= '80'>";
+       }else{
+           $hinh = "no photo";
+       }
+ 
+
+
+?>
+
+<div class="row">
+            <div class="row formtilte">
+                <h1>CẬP NHẬP LOẠI HÀNG HOÁ</h1>
+            </div>
+            <div class="row formconten">
+            <form action="index.php?act=updatesp" method="post" enctype="multipart/form-data" >
+                    <div class="row mb10">
+                    <select name="iddm" id="">
+                            <option value="0" selected>Tất cả</option>
+                        <?php
+                                foreach ($listdanhmuc as $danhmuc) {
+                                    extract($danhmuc);
+                                    if($iddm == $id)
+                                        echo '<option value="'.$id.'" selected>'.$name.'</option>';
+                                    else echo '<option value="'.$id.'">'.$name.'</option>';
+                                }
+                            ?>
+
+                        </select>
+                    </div>
+                    <div class="row mb10">
+                        Tên sản phẩm: <br>
+                        <input type="text" name="tensp" value="<?=$name?>" >
+                    </div>
+                    <div class="row mb10">
+                        Giá: <br>
+                        <input type="text" name="giasp" value="<?=$price?>">
+                    </div>
+                    <div class="row mb10">
+                        Hình <br>
+                        <input type="file" name="hinh" >
+                        <?= $hinh ?>
+                    </div>
+                    <div class="row mb10">
+                        Mô tả: <br>
+                        <textarea name="mota" rows="10" cols="30"><?=$mota?></textarea>
+                    </div>
+                    <div class="row mb10">
+                        <input type="hidden" name="id" value="<?= $id ?>">
+                        <input type="submit" name="capnhap" value="CẬP NHẬP">   
+                        <input type="reset" value="NHẬP LẠI" >
+                        <a href="index.php?act=listsp"><input type="button" value="DANH SÁCH" ></a>
+                    </div>
+                    <?php 
+                        if(isset($thongbao) && ($thongbao != ""))
+                        echo $thongbao;
+                    ?>
+                </form>
+            </div>
+        </div>
+    </div>
