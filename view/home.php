0e2f089703ea7643ce0f0678ad0e92fb99fe239f
duonglmph41308 <duongluong2k4@gmail.com>
2024-02-19 13:15:31 +0700

    new

--- view/home.php
+++ view/home.php
@@ -40,39 +40,59 @@
         </div>
         <div class="row">
             <?php
-                    $i=0;
-                        foreach ($spnew as $sp) {
-                            extract($sp);
-                            $linksp="index.php?act=sanphamct&idsp=".$id;
-                            $hinh = $img_path.$img;
-                            if(($i == 2) || ($i == 5) || ($i == 8)){
-                                $mr = "";
-                            }else{
-                                $mr = "mr";
-                            }
-                            echo '<div class="boxsp '.$mr.' ">
-                                    <div class="row img "><a href="'.$linksp.'"><img src="'.$hinh.'" alt=""></a></div>
-                                    <p>$'.$price.'</p>
-                                    <a href="'.$linksp.'">'.$name.'</a>
+            $i = 0;
+            foreach ($spnew as $sp) {
+                extract($sp);
+                $linksp = 'index.php?act=sanphamct&idsp=' . $id;
+                $hinh = $img_path . $img;
+                if ($i == 2 || $i == 5 || $i == 8) {
+                    $mr = '';
+                } else {
+                    $mr = 'mr';
+                }
+                echo '<div class="boxsp ' .
+                    $mr .
+                    ' ">
+                                    <div style="margin-left: 15px;" class="row img "><a href="' .
+                    $linksp .
+                    '"><img src="' .
+                    $hinh .
+                    '" alt=""></a></div>
+                                    <p>$' .
+                    $price .
+                    '</p>
+                                    <a href="' .
+                    $linksp .
+                    '">' .
+                    $name .
+                    '</a>
                                     <div class="row mb btnaddtocart">
                                         <form action="index.php?act=addtocart" method="post">
-                                            <input type="hidden" name="id" value="'.$id.'">
-                                            <input type="hidden" name="name" value="'.$name.'">
-                                            <input type="hidden" name="img" value="'.$img.'">
-                                            <input type="hidden" name="price" value="'.$price.'">
+                                            <input type="hidden" name="id" value="' .
+                    $id .
+                    '">
+                                            <input type="hidden" name="name" value="' .
+                    $name .
+                    '">
+                                            <input type="hidden" name="img" value="' .
+                    $img .
+                    '">
+                                            <input type="hidden" name="price" value="' .
+                    $price .
+                    '">
                                             <input type="submit" name="addtocart" value="Thêm vào giỏ hàng">
                                         </form>
                                     </div>
                                 </div>';
-                                $i+=1;
-                        }
-                    ?>
+                $i += 1;
+            }
+            ?>
         </div>
     </div>
 
     <!-- end trai -->
     <div class="boxphai">
-        <?php include "boxright.php"; ?>
+        <?php include 'boxright.php'; ?>
     </div>
     <!-- end phai -->
 </div>
